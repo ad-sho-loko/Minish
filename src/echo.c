747ec379d88561bca5b7a16c02c64399a33dbfde
ad-sho-loko <ad.sho.loko@gmail.com>
2018-04-17 22:29:33 +0900

    impl very simple echo

--- src/echo.c
+++ src/echo.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int main(void){
-  puts("Yeaaaaaaah!");
+int main(int argc, char** argv){
+
+  for(char **p = argv + 1; *p; p++){
+    printf("%s ", *p);
+  }
+  
+  printf("\n");
   return 0;
 }
